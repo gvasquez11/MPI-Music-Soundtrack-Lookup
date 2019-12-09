a3e986bca5818187b724c47ccaf7b8cf95f83f17
Gregory Vasquez <vasquez.greg16@gmail.com>
2019-12-08 19:22:44 -0800

    begin read in portion

--- LookUp.cpp
+++ LookUp.cpp
@@ -1,10 +1,13 @@
 #include <iomanip>
 #include <string>
 #include <iostream>
+#include <mpi.h>
 
 using namespace std;
-
+int ROWS = 4;
+int COLS = 2;
 int CAPACITY = 20;
+string FNAME = "in.txt";
 
 struct Song
 {
@@ -19,9 +22,40 @@ class LookUp
 {
 
 public:
-  LookUp(string fileName); //Construcotr to automatically read input fileName
+  LookUp(string fileName) //Construcotr to automatically read input fileName
+  {
+    //fileName = FNAME;
+    int rank = 0;
+    MPI_Comm comm = MPI_COMM_WORLD;
+
+    // Initialize MPI and get the process rank
+    MPI_Init(NULL,NULL);
+    MPI_Comm_rank(comm,&rank);
+
+    // Build the filename for the given process
+    string filename = "input_" + rank + ".txt";
+
+    // Open the file stream and read or write
+    ifstream in(filename.c_str());
+    read_file(in);
+    in.close();
+
+    // Finalize MPI and exit
+    MPI_Finalize();
+  }
 
 private:
-  Song songArr* = new Song[CAPACITY];
+  Song * songInfoArr = new Song[CAPACITY];
+
+};
+
+int main()
+{
+
+
+
+
+
+
 
 }
--- README.md
+++ README.md
@@ -1,1 +1,8 @@
 # MPI-Music-Soundtrack-Lookup
+Gregory Vasquez gvasquez11@csu.fullerton.edu
+
+This program is written in C++ MPI which processes lines in order to output a value.
+Each line of the 'in.txt' contains a string that represents concatenated musical notes from
+famous Star Wars songs. This program will read in these lines and scan them to determine
+which of these famous Star Wars songs is represented. Once it determines which song it is,
+it will output a time stamp of where you can find the song in the it's respective film. 
