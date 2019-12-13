d52a7ca63219a06ffea71ed0fd3f7ee6d9e80908
Yashab Narang <yuvio100@csu.fullerton.edu>
2019-12-13 13:30:38 -0800

    sends song on line x to rank x

--- main.cpp
+++ main.cpp
@@ -20,26 +20,24 @@ int main(int argc, char** argv)
   LookUp test("data.txt");
   if(rank == 0)
   {
-    printf("%d of %d:\n", rank, size-1);
+    //printf("%d of %d:\n", rank, size-1);
 
     //LookUp test("data.txt");
     test.readIn("in.txt");
     cout << "The number of songs found in file are: " << test.getNumOfSongs() << endl;
 
-    for(int i = 0; i < size; i++)
+    for(int i = 0; i < size-1; i++)
     {
       message = test.getInVect().at(i);
-      MPI_Send(&message[0], message.size()+1, MPI_BYTE, i, /*TAG:*/0, MPI_COMM_WORLD);
+      MPI_Send(&message[0], message.size()+1, MPI_BYTE, i+1, /*TAG:*/0, MPI_COMM_WORLD);
     }
     //test.checkSong(test.getSongVec(), test.getInVect());
     //cout << test.getInVect().at(3) << endl;
 
-    printf("%d of %d Complete!\n", rank, size-1);
+    //printf("%d of %d Complete!\n", rank, size-1);
   }
   else
   {
-    printf("%d of %d:\n", rank, size-1);
-
     //START code from https://stackoverflow.com/questions/21378302/how-to-send-stdstring-in-mpi?rq=1
     MPI_Status status;
     MPI_Probe(0,MPI_ANY_TAG,MPI_COMM_WORLD,&status);
@@ -54,11 +52,11 @@ int main(int argc, char** argv)
 
     //Just to test the vector m (you can comment it later)
     //for (int i = 0; i < m.size(); i++)
-      //  cout << m[i] << "\n";
+      //cout << "Rank " << rank << " holds " << m[i] << "\n";
 
     test.checkSong(test.getSongVec(), m);
 
-    printf("%d of %d Complete!\n", rank, size-1);
+    //printf("%d of %d Complete!\n", rank, size-1);
   }
 
   MPI_Finalize();
